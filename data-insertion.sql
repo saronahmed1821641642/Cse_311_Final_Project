48b002379a7a7f542991fe085883f82efe5d5f4e
IhsanKabir <87293412+IhsanKabir@users.noreply.github.com>
2021-09-20 04:00:45 +0600

    Add files via upload

---
+++ data-insertion.sql
@@ -0,0 +1,69 @@
+insert into Administrator
+ values
+('100','Gordon'),
+('101','Matt'),
+('102','Gary'),
+('103','Matts'),
+('104','Reynold'),
+('105','Diana')
+
+select * from Administrator
+
+insert into Registration
+ values
+('115','Emilia','Jackson','1983-04-15','01892234122','emilia124@gmail.com','emilia970000'),
+('116','Benn','Ungerman','1982-08-09','01892234123','ben124@gmail.com','ben560000'),
+('117','Reece','Hignel','1986-01-19','01892234124','Reece124@gmail.com','reece107000'),
+('118','rose','adam','1969-12-11','01892234125','rose124@gmail.com','rose530000'),
+('119','Karlie','Verkerk','1989-06-06','01892234126','karlie124@gmail.com','karlie780000')
+
+insert into Users
+ values
+('115','Emilia','Jackson','1983-04-15','01892234122','emilia124@gmail.com'),
+('116','Benn','Ungerman','1982-08-09','01892234123','ben124@gmail.com'),
+('117','Reece','Hignel','1986-01-19','01892234124','Reece124@gmail.com'),
+('118','rose','adam','1969-12-11','01892234125','rose124@gmail.com'),
+('119','Karlie','Verkerk','1989-06-06','01892234126','karlie124@gmail.com')
+
+insert into Login
+ values
+ ('115', 'emilia970000'),
+('116','ben560000'),
+('117','reece107000'),
+('118','rose530000'),
+('119','karlie780000')
+
+insert into forget_password
+ values
+('115', 'emilia970000', 'emilia1234'),
+('116','ben560000''ben1234'),
+('117','reece107000''reece1234'),
+('118','rose530000''rosea1234'),
+('119','karlie780000''karlie1234')
+
+insert into Payment
+ values 
+('115', '970000', '0121215484544521'),
+('116','560000''01212154845445255'),
+('117','107000''01212154845445247'),
+('118','530000''01212154845445247'),
+('119','780000''01212154845445123')
+
+insert into kid
+ values 
+ ('128','2007-05-12'),
+('129','2008-04-19'),
+('130','2009-01-22')
+
+
+insert into kid
+ values 
+ ('115','1996-05-12'),
+('116','1999-04-19'),
+('116','1994-01-22')
+
+insert into kid
+ values 
+ ('The Jungle Book', 'Adventure/Family', 3024.27, 116:25, 'Neel Sethi, Bill Murray, Ben Kingsley, Idris Elba.', 9.7, '115', '100'),
+('Avengers', 'superhero/fantasy/action', 5005.28, 143:0, 'Robert Downey Jr. · Chris Evans · Mark
+Ruffalo · Chris Hemsworth · Scarlett Johansson · Jeremy Renner · Tom Hiddleston · Clark Gregg.', 10.0, '115', '100'),
